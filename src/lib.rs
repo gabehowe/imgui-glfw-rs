cc7ecf06d86ddb2c101bca93c7d277782845fb7c
Gabriel Howe <gabriel@jesshowe.com>
2024-10-17 01:22:35 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,282 +1,233 @@
-extern crate gl;
-extern crate glfw;
+//! ImGui input handling for Glfw.
+//!
+//! # Example use
+//! You can run this example with `cargo run --example hello_world`
+//!
+//! ```rust
+//! use glfw::Context;
+//! use imgui::Context as ImContext;
+//! use imgui_glfw_rs::glfw;
+//! use imgui_glfw_rs::imgui;
+//! use imgui_glfw_rs::ImguiGLFW;
+//!
+//! fn main() {
+//!     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+//!     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+//!
+//!     let (mut window, events) = glfw
+//!         .create_window(
+//!             1024,
+//!             768,
+//!             "imgui-glfw-rs example",
+//!             glfw::WindowMode::Windowed,
+//!         )
+//!         .expect("Failed to create window");
+//!
+//!     window.make_current();
+//!     window.set_all_polling(true);
+//!
+//!     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+//!     unsafe {
+//!         gl::Enable(gl::BLEND);
+//!         gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+//!         gl::Enable(gl::DEPTH_TEST);
+//!         gl::DepthFunc(gl::LESS);
+//!         gl::ClearColor(0.1, 0.1, 0.1, 1.0);
+//!     }
+//!
+//!     let mut imgui = ImContext::create();
+//!
+//!     let mut imgui_glfw = ImguiGLFW::new(&mut imgui, &mut window);
+//!
+//!     while !window.should_close() {
+//!         unsafe {
+//!             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+//!         }
+//!
+//!         let ui = imgui_glfw.frame(&mut window, &mut imgui);
+//!
+//!         ui.show_demo_window(&mut true);
+//!
+//!         imgui_glfw.draw(ui, &mut window);
+//!
+//!         window.swap_buffers();
+//!
+//!         glfw.poll_events();
+//!         for (_, event) in glfw::flush_messages(&events) {
+//!             imgui_glfw.handle_event(&mut imgui, &event);
+//!         }
+//!     }
+//! }
+//! ```
 
-use std::io::Read;
-use std::ptr;
+/// Use the reexported glfw crate to avoid version conflicts.
+pub use glfw;
+/// Use the reexported imgui crate to avoid version conflicts.
+pub use imgui;
 
-use cgmath::{InnerSpace, Vector3};
-use gl::{COLOR_BUFFER_BIT, DEBUG_OUTPUT, DEBUG_OUTPUT_SYNCHRONOUS, DEBUG_SEVERITY_NOTIFICATION, DEBUG_SOURCE_API, DEBUG_TYPE_ERROR, DEPTH_BUFFER_BIT, DEPTH_TEST, FILL, FRONT_AND_BACK};
-use glfw::{Action, Context, CursorMode, fail_on_errors, Glfw, GlfwReceiver, Key, PWindow, WindowEvent, WindowHint};
-use glfw::ffi::*;
-use imgui::*;
+use glfw::ffi::GLFWwindow;
+use glfw::{Action, Key, Modifiers, MouseButton, PWindow, StandardCursor, WindowEvent};
+use imgui::{ConfigFlags, Context, Key as ImGuiKey, MouseCursor, Ui};
+use imgui_opengl_renderer::Renderer;
+use std::ffi::{CStr, CString};
+use std::os::raw::{c_char, c_void};
+use std::time::Instant;
 
-use renderable::Renderable;
-use transformation::Camera;
-use util::debug_log;
+struct GlfwClipboardBackend(*mut c_void);
 
-pub mod util;
-pub mod transformation;
-pub mod renderable;
+impl imgui::ClipboardBackend for GlfwClipboardBackend {
+    fn get(&mut self) -> Option<String> {
+        let char_ptr = unsafe { glfw::ffi::glfwGetClipboardString(self.0 as *mut GLFWwindow) };
+        let c_str = unsafe { CStr::from_ptr(char_ptr) };
+        Some(imgui::ImString::new(c_str.to_str().unwrap()).to_string())
+    }
 
-const HEIGHT: u32 = 1000;
-const WIDTH: u32 = HEIGHT * 16 / 9;
-const MOVESPEED: f32 = 0.025;
-const ROTATIONSPEED: f32 = 0.025;
-
-pub struct Data {
-    pub(crate) renderables: Vec<Renderable>,
-    camera: Camera,
+    fn set(&mut self, value: &str) {
+        unsafe {
+            glfw::ffi::glfwSetClipboardString(self.0 as *mut GLFWwindow, value.to_string().as_ptr() as *const c_char);
+        };
+    }
 }
 
-impl Data {
-    unsafe fn render(&mut self) {
-        gl::ClearColor(0.0, 0.0, 0.2, 1.0);
-        gl::Clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
-        self.camera.update_buffers();
-        // unsafe {renderables[0].shader.set_vec3(Vector3::new(255.0, 0.0, 0.0), "ourColor");}
-        // unsafe {renderables[1].shader.set_vec3(Vector3::new(0.0, 0.0, 255.0), "ourColor");}
-
-        for i in &mut *self.renderables {
-            i.render();
-        }
-    }
-
-    unsafe fn handle_input(&mut self, window: &PWindow) {
-        let mut speed = MOVESPEED;
-        if glfwGetKey(window.window_ptr(), KEY_LEFT_SHIFT) == PRESS {
-            speed *= 3.0;
-        }
-        if glfwGetKey(window.window_ptr(), KEY_D) == PRESS {
-            self.camera.pos += self.camera.front.cross(Vector3::new(0.0, 1.0, 0.0)).normalize() * speed;
-        }
-        if glfwGetKey(window.window_ptr(), KEY_A) == PRESS {
-            self.camera.pos -= self.camera.front.cross(Vector3::new(0.0, 1.0, 0.0)).normalize() * speed;
-        }
-
-        if glfwGetKey(window.window_ptr(), KEY_S) == PRESS {
-            self.camera.pos -= self.camera.front * speed;
-        }
-        if glfwGetKey(window.window_ptr(), KEY_W) == PRESS {
-            self.camera.pos += self.camera.front * speed;
-        }
-
-        if glfwGetKey(window.window_ptr(), KEY_E) == PRESS {
-            self.camera.translate(Vector3::new(0.0, speed, 0.0));
-        }
-        if glfwGetKey(window.window_ptr(), KEY_Q) == PRESS {
-            self.camera.translate(Vector3::new(0.0, -speed, 0.0));
-        }
-
-
-        if glfwGetKey(window.window_ptr(), KEY_LEFT) == PRESS {
-            self.camera.yaw -= ROTATIONSPEED;
-        }
-        if glfwGetKey(window.window_ptr(), KEY_RIGHT) == PRESS {
-            self.camera.yaw += ROTATIONSPEED;
-        }
-        if glfwGetKey(window.window_ptr(), KEY_UP) == PRESS {
-            self.camera.pitch += ROTATIONSPEED;
-        }
-        if glfwGetKey(window.window_ptr(), KEY_DOWN) == PRESS {
-            self.camera.pitch -= ROTATIONSPEED;
-        }
-    }
-
-    pub fn get_renderable(&self, index: usize) -> &Renderable {
-        return &self.renderables[index];
-    }
-    pub fn get_renderable_mut(&mut self, index: usize) -> &mut Renderable {
-        return &mut self.renderables[index];
-    }
+pub struct ImguiGLFW {
+    last_frame: Instant,
+    mouse_press: [bool; 5],
+    cursor_pos: (f64, f64),
+    cursor: (MouseCursor, Option<StandardCursor>),
+    renderer: Renderer,
 }
 
-
-pub(crate) struct Engine {
-    glfw: Glfw,
-    window: PWindow,
-    events: GlfwReceiver<(f64, WindowEvent)>,
-    pub frametime: f64,
-    pub data: Data,
-    pub event_handler: EventHandler,
-}
-
-impl Engine {
-    pub fn new(imgui: bool) -> Engine {
-        let (glfw, mut window, events) = init_gflw();
-        let camera = Engine::init_gl();
+impl ImguiGLFW {
+    pub fn get_renderer(&self) -> &Renderer {
+        return &self.renderer;
+    }
+    pub fn new(imgui: &mut Context, window: &mut PWindow) -> Self {
         unsafe {
-            gl::GetString(gl::VERSION);
+            let window_ptr = glfw::ffi::glfwGetCurrentContext() as *mut c_void;
+            imgui.set_clipboard_backend((GlfwClipboardBackend(window_ptr)));
         }
-        let mut event_handler = EventHandler::raw();
-        if imgui {
-            event_handler = EventHandler::new(&mut window);
-        }
-        unsafe {
-            gl::GetString(gl::RENDERER);
-        }
-        Engine {
-            glfw,
-            window,
-            events,
-            frametime: 0.0,
-            data: Data {
-                renderables: vec![],
-                camera,
-            },
-            event_handler,
+
+        let mut io_mut = imgui.io_mut();
+        io_mut.key_map[ImGuiKey::Tab as usize] = Key::Tab as u32;
+        io_mut.key_map[ImGuiKey::LeftArrow as usize] = Key::Left as u32;
+        io_mut.key_map[ImGuiKey::RightArrow as usize] = Key::Right as u32;
+        io_mut.key_map[ImGuiKey::UpArrow as usize] = Key::Up as u32;
+        io_mut.key_map[ImGuiKey::DownArrow as usize] = Key::Down as u32;
+        io_mut.key_map[ImGuiKey::PageUp as usize] = Key::PageUp as u32;
+        io_mut.key_map[ImGuiKey::PageDown as usize] = Key::PageDown as u32;
+        io_mut.key_map[ImGuiKey::Home as usize] = Key::Home as u32;
+        io_mut.key_map[ImGuiKey::End as usize] = Key::End as u32;
+        io_mut.key_map[ImGuiKey::Insert as usize] = Key::Insert as u32;
+        io_mut.key_map[ImGuiKey::Delete as usize] = Key::Delete as u32;
+        io_mut.key_map[ImGuiKey::Backspace as usize] = Key::Backspace as u32;
+        io_mut.key_map[ImGuiKey::Space as usize] = Key::Space as u32;
+        io_mut.key_map[ImGuiKey::Enter as usize] = Key::Enter as u32;
+        io_mut.key_map[ImGuiKey::Escape as usize] = Key::Escape as u32;
+        io_mut.key_map[ImGuiKey::A as usize] = Key::A as u32;
+        io_mut.key_map[ImGuiKey::C as usize] = Key::C as u32;
+        io_mut.key_map[ImGuiKey::V as usize] = Key::V as u32;
+        io_mut.key_map[ImGuiKey::X as usize] = Key::X as u32;
+        io_mut.key_map[ImGuiKey::Y as usize] = Key::Y as u32;
+        io_mut.key_map[ImGuiKey::Z as usize] = Key::Z as u32;
+
+        let renderer = Renderer::new(imgui, |s| window.get_proc_address(s) as _);
+
+        Self {
+            last_frame: Instant::now(),
+            mouse_press: [false; 5],
+            cursor_pos: (0., 0.),
+            cursor: (MouseCursor::Arrow, None),
+
+            renderer,
         }
     }
-    pub(crate) fn add_renderable(&mut self, mut renderable: Renderable) {
-        unsafe { renderable.shader.bind_matrices(); }
-        self.data.renderables.push(renderable);
-    }
 
-    fn init_gl() -> Camera {
-        // unsafe { gl::PolygonMode(FRONT_AND_BACK, LINE); }
-        let mut camera = Camera::new();
-        unsafe {
-            camera.initialize_buffers();
-            gl::CullFace(gl::BACK);
-            gl::Enable(gl::TEXTURE_2D);
-        }
-        return camera;
-    }
-
-    pub(crate) fn should_keep_running(&mut self) -> bool {
-        !self.window.should_close()
-    }
-    pub(crate) fn update(&mut self, imgui_callback: fn(&mut Ui)) {
-        unsafe {
-            self.data.handle_input(&self.window);
-            self.data.render();
-        }
-        // Allow for disabling imgui
-        if self.event_handler.imgui.is_some() {
-            let imgui_glfw_ref = self.event_handler.imgui_glfw.as_mut().unwrap();
-            let imgui_ref = self.event_handler.imgui.as_mut().unwrap();
-            let mut frame = imgui_ref.frame();
-            imgui_callback(&mut frame);
-            imgui_glfw_ref.draw(frame, &mut self.window);
-            imgui_glfw_ref.get_renderer().render(imgui_ref);
-        }
-
-        self.window.swap_buffers();
-        self.glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&self.events) {
-            // println!("{:?}", event);
-            if self.event_handler.imgui.is_some() {
-                self.event_handler.imgui_glfw.as_mut().unwrap().handle_event(self.event_handler.imgui.as_mut().unwrap(), &event);
+    pub fn handle_event(&mut self, imgui: &mut Context, event: &WindowEvent) {
+        match *event {
+            WindowEvent::MouseButton(mouse_btn, action, _) => {
+                let index = match mouse_btn {
+                    MouseButton::Button1 => 0,
+                    MouseButton::Button2 => 1,
+                    MouseButton::Button3 => 2,
+                    MouseButton::Button4 => 3,
+                    MouseButton::Button5 => 4,
+                    _ => 0,
+                };
+                let press = action != Action::Release;
+                self.mouse_press[index] = press;
+                imgui.io_mut().mouse_down = self.mouse_press;
             }
-            match event {
-                WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    self.window.set_should_close(true);
-                }
-                WindowEvent::Key(Key::K, _, Action::Press, _) => unsafe {
-                    if self.window.get_cursor_mode() == CursorMode::Disabled {
-                        self.window.set_cursor_mode(CursorMode::Normal)
-                    } else {
-                        self.window.set_cursor_mode(CursorMode::Disabled)
-                    }
-                    self.window.make_current()
-                }
-                WindowEvent::Key(Key::F2, _, Action::Press, _) => {
-                    if !self.event_handler.wireframe {
-                        unsafe { gl::PolygonMode(FRONT_AND_BACK, gl::LINE) };
-                    } else {
-                        unsafe { gl::PolygonMode(FRONT_AND_BACK, FILL); }
-                    }
-                    println!("Wireframe: {}", self.event_handler.wireframe);
-                    self.event_handler.wireframe = !self.event_handler.wireframe;
-                }
-                WindowEvent::CursorPos(x, y) => unsafe {
-                    // println!("{}, {}", x, y);
-                    if (self.window.get_cursor_mode() == CursorMode::Disabled) {
-                        self.data.camera.handle_mouse(x, y);
-                    }
-                    // unsafe { glfwSetCursorPos(window.window_ptr(), (WIDTH / 2) as c_double, HEIGHT as c_double / 2.0); }
-                }
-                _ => {}
+            WindowEvent::CursorPos(w, h) => {
+                imgui.io_mut().mouse_pos = [w as f32, h as f32];
+                self.cursor_pos = (w, h);
             }
+            WindowEvent::Scroll(_, d) => {
+                imgui.io_mut().mouse_wheel = d as f32;
+            }
+            WindowEvent::Char(character) => {
+                imgui.io_mut().add_input_character(character);
+            }
+            WindowEvent::Key(key, _, action, modifier) => {
+                Self::set_mod(imgui, modifier);
+                imgui.io_mut().keys_down[key as usize] = action != Action::Release;
+            }
+            _ => {}
         }
-
-        self.event_handler.current_frame_time = unsafe { glfwGetTime() };
-        self.frametime = self.event_handler.current_frame_time - self.event_handler.last_frame_time;
-        self.event_handler.last_frame_time = self.event_handler.current_frame_time;
-        // println!("Frame rate: {}", frame_rate);
     }
-}
 
-pub struct EventHandler {
-    wireframe: bool,
-    current_frame_time: f64,
-    last_frame_time: f64,
-    imgui: Option<imgui::Context>,
-    imgui_glfw: Option<imgui_glfw_rs::ImguiGLFW>,
-}
-impl EventHandler {
-    fn new(window: &mut PWindow) -> EventHandler {
-        let mut wireframe = false;
-        // let mut renderer = imgui_opengl_renderer::Renderer::new(&mut ctx, |s| self.window.get_proc_address(s) as _);
+    pub fn frame(&'static mut self, window: &mut PWindow, imgui: &'static mut Context) -> &mut Ui {
+        let io = imgui.io_mut();
 
-        let mut imgui = imgui::Context::create();
-        let mut imgui_glfw = imgui_glfw_rs::ImguiGLFW::new(&mut imgui, window);
+        let now = Instant::now();
+        let delta = now - self.last_frame;
+        let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
+        self.last_frame = now;
+        io.delta_time = delta_s;
+
         let window_size = window.get_size();
         println!("Window Size: {:?}", window_size);
-        imgui.io_mut().display_size = [window_size.0 as f32, window_size.1 as f32];
-        EventHandler {
-            wireframe,
-            current_frame_time: 0.0,
-            last_frame_time: unsafe { glfwGetTime() },
-            imgui: Some(imgui),
-            imgui_glfw: Some(imgui_glfw),
+        io.display_size = [window_size.0 as f32, window_size.1 as f32];
+
+        let ui = imgui.frame();
+        return ui;
+    }
+
+    pub fn draw<'ui>(&mut self, ui: &mut Ui, window: &mut PWindow) {
+        let io = ui.io();
+        if !io
+            .config_flags
+            .contains(ConfigFlags::NO_MOUSE_CURSOR_CHANGE)
+        {
+            match ui.mouse_cursor() {
+                Some(mouse_cursor) if !io.mouse_draw_cursor => {
+                    // window.set_cursor_mode(glfw::CursorMode::Normal);
+                    let cursor = match mouse_cursor {
+                        MouseCursor::TextInput => StandardCursor::IBeam,
+                        MouseCursor::ResizeNS => StandardCursor::VResize,
+                        MouseCursor::ResizeEW => StandardCursor::HResize,
+                        MouseCursor::Hand => StandardCursor::Hand,
+                        _ => StandardCursor::Arrow,
+                    };
+                    window.set_cursor(Some(glfw::Cursor::standard(cursor)));
+
+                    if self.cursor.1 != Some(cursor) {
+                        self.cursor.1 = Some(cursor);
+                        self.cursor.0 = mouse_cursor;
+                    }
+                }
+                _ => {
+                    self.cursor.0 = MouseCursor::Arrow;
+                    self.cursor.1 = None;
+                    // window.set_cursor_mode(glfw::CursorMode::Hidden);
+                }
+            }
         }
     }
-    fn raw() -> Self {
-        EventHandler {
-            wireframe: false,
-            current_frame_time: 0.0,
-            last_frame_time: 0.0,
-            imgui: None,
-            imgui_glfw: None,
-        }
+
+    fn set_mod(imgui: &mut Context, modifier: Modifiers) {
+        imgui.io_mut().key_ctrl = modifier.intersects(Modifiers::Control);
+        imgui.io_mut().key_alt = modifier.intersects(Modifiers::Alt);
+        imgui.io_mut().key_shift = modifier.intersects(Modifiers::Shift);
+        imgui.io_mut().key_super = modifier.intersects(Modifiers::Super);
     }
 }
-
-fn init_gflw() -> (Glfw, PWindow, GlfwReceiver<(f64, WindowEvent)>) {
-    use glfw::fail_on_errors;
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
-
-    let (mut window, events) = glfw
-        .create_window(WIDTH, HEIGHT, "OpenGL", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
-
-    glfw.window_hint(WindowHint::ContextVersionMajor(4));
-    glfw.window_hint(WindowHint::ContextVersionMinor(6));
-    glfw.window_hint(WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-    glfw.window_hint(WindowHint::OpenGlDebugContext(true));
-
-    window.make_current();
-    window.set_framebuffer_size_polling(true);
-    window.set_key_polling(true);
-    window.set_cursor_pos_polling(true);
-    window.set_mouse_button_polling(true);
-    window.set_scroll_polling(true);
-
-    gl::load_with(|s| glfw.get_proc_address_raw(s));
-    unsafe {
-        glfwSetInputMode(window.window_ptr(), CURSOR, CURSOR_DISABLED);
-        gl::Enable(DEPTH_TEST);
-        gl::Enable(DEBUG_OUTPUT);
-        gl::Enable(DEBUG_OUTPUT_SYNCHRONOUS);
-        // gl::Enable(PROGRAM_POINT_SIZE);
-        gl::DebugMessageControl(DEBUG_SOURCE_API, DEBUG_TYPE_ERROR, DEBUG_SEVERITY_NOTIFICATION, 0, ptr::null(), gl::TRUE);
-        gl::DebugMessageCallback(Some(debug_log), ptr::null())
-    }
-    (glfw, window, events)
-}
-
-
-
-
-
-
